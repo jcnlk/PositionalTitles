b5b6b2a79e4e52bff163a49a346d609fa75834d8
jcnlk <itsjxnik@gmail.com>
2025-03-08 04:48:37 +0100

    Add party messages to trigger a title

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const lastLocation = {
   AtEE2: false,
   AtEE3: false,
   AtCore: false,
-  InCore: false,
-  AtMid: false
+  InGoldorTunnel: false,
+  AtMid: false,
+  Ati4Entry: false,
+  AtP5: false
 }
 
 const ClassColor = {
@@ -23,6 +25,34 @@ const ClassColor = {
   Berserk: "&c",
   Archer: "&6"
 };
+
+// At P2
+const validP2Msg = ["at p2", "in p2"];
+const isValidP2 = playerInfo => !lastLocation.AtP2 && isInBoss("Maxor") && playerInfo.className !== "Healer";
+// At SS
+const validAtSSMsg = ["at ss", "at simon says"];
+const isValidAtSS = () => !lastLocation.AtSS && (isInBoss("Storm") || isInBoss("Goldor"));
+// At EE2
+const validEE2Msg = ["early enter 2", "pre enter 2", "at ee2", "entered 3.2"];
+const isValidEE2 = () => !lastLocation.AtEE2 && currentGoldorPhase === 1;
+// At EE3
+const validEE3Msg = ["early enter 3", "pre enter 3", "at ee3", "entered 3.3"];
+const isValidEE3 = () => !lastLocation.AtEE3 && currentGoldorPhase === 2;
+// At Core
+const validAtCoreMsg = ["at core", "pre enter 4", "early enter 4", "at ee4", "entered 3.4"];
+const isValidAtCore = () => !lastLocation.AtCore && (currentGoldorPhase === 2 || currentGoldorPhase === 3);
+// In Goldor Tunnel
+const validInGoldorTunnelMsg = ["in goldor tunnel", "inside goldor tunnel", "in core", "entered 3.5", "at ee5", "at pre enter 5"];
+const isValidInGoldorTunnel = () => !lastLocation.InGoldorTunnel && currentGoldorPhase === 4;
+// At Mid
+const validAtMidMsg = ["at mid", "in mid"];
+const isValidAtMid = () => !lastLocation.AtMid && isInBoss("Necron");
+//At i4 Entry
+const validAti4EntryMsg = ["i4 entry"];
+const isValidAti4Entry = playerInfo => !lastLocation.Ati4Entry && isInBoss("Storm") && playerInfo.className === "Healer";
+// At P5
+const validAtP5Msg = ["at p5", "in p5"];
+const isValidAtP5 = () => !lastLocation.AtP5 && isInBoss("Necron");
 
 function playSound() {
   let count = 0;
@@ -53,12 +83,12 @@ function showTitle(text) {
   setTimeout(() => overlay.unregister(), 2000);
 };
 
-function showAlert(text) {
-  ChatLib.chat(`${PREFIX} ` + text)
-  showTitle(text);
+function showAlert(playerName, playerInfo, text) {
+  ChatLib.chat(`${PREFIX} ${playerInfo.color}${playerName} (${playerInfo.className[0]}) &e${text}`);
+  showTitle(`${playerInfo.color}${playerName} (${playerInfo.className[0]}) &e${text}`);
 }
 
-function isPlayerInArea(x1, x2, y1, y2, z1, z2, entity = Player.getPlayer()) {
+function isPlayerInArea(x1, x2, y1, y2, z1, z2, entity = Player) {
   const x = entity.getX();
   const y = entity.getY();
   const z = entity.getZ();
@@ -82,28 +112,10 @@ function getClassOf(name) {
   return { color: ClassColor[className] || "&f", className };
 };
 
-function getIsInMaxor() {
+function isInBoss(boss) {
   const bossName = BossStatus.field_82827_c;
   if (!bossName) return false;
-  return bossName.removeFormatting().includes("Maxor");
-}
-
-function getIsInStorm() {
-  const bossName = BossStatus.field_82827_c;
-  if (!bossName) return false;
-  return bossName.removeFormatting().includes("Storm");
-}
-
-function getIsInGoldor() {
-  const bossName = BossStatus.field_82827_c;
-  if (!bossName) return false;
-  return bossName.removeFormatting().includes("Goldor");
-}
-
-function getIsInNecron() {
-  const bossName = BossStatus.field_82827_c;
-  if (!bossName) return false;
-  return bossName.removeFormatting().includes("Necron");
+  return bossName.removeFormatting().includes(boss);
 }
 
 register("chat", message => {
@@ -111,56 +123,100 @@ register("chat", message => {
   if ((message.includes("(7/7)") || message.includes("(8/8)")) && !message.includes(":")) currentGoldorPhase += 1;
 }).setCriteria("${message}");
 
-register("step", () => {
-  if (!World.isLoaded || !config.toggled) return;
+register("tick", () => {
+  if (!World.isLoaded) return;
+  if (!config.toggled) return;
 
   World.getAllPlayers().forEach(entity => {
-    if (entity.isInvisible() || entity.getPing() !== 1 || entity.getName() === Player.getName()) return;
-
+    if (entity.getPing() !== 1) return; // Skip non-player entities
     const playerName = entity.getName();
+    if (playerName === Player.getName()) return; // Skip us
     const playerInfo = getClassOf(playerName);
-    const isInMaxor = getIsInMaxor();
-    const isInStorm = getIsInStorm();
-    const isInGoldor = getIsInGoldor();
-    const isInNecron = getIsInNecron();
+    if (!playerInfo.className) return; // not in dungeon
 
-    if (entity.getY() < 205 && entity.getY() > 164 && !lastLocation.AtP2 && isInMaxor && playerInfo.className !== "Healer") {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt P2!`;
+    if (entity.getY() < 205 && entity.getY() > 164 && !lastLocation.AtP2 && isInBoss("Maxor") && playerInfo.className !== "Healer") {
       lastLocation.AtP2 = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At P2!");
     }
-    if (isPlayerInArea(106, 110, 118, 122, 92, 96, entity) && !lastLocation.AtSS && (isInStorm || isInGoldor)) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt SS!`;
+    if (isPlayerInArea(106, 110, 118, 122, 92, 96, entity) && !lastLocation.AtSS && (isInBoss("Storm") || isInBoss("Goldor"))) {
       lastLocation.AtSS = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At SS!");
     }
-    if (isPlayerInArea(52, 56, 107, 111, 129, 133, entity) && !lastLocation.AtEE2 && currentGoldorPhase === 1 && isInGoldor) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt EE2!`;
+    if (isPlayerInArea(49, 58, 108, 115, 129, 133, entity) && !lastLocation.AtEE2 && currentGoldorPhase === 1) {
       lastLocation.AtEE2 = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At Pre Enter 2!");
     }
-    if (isPlayerInArea(0, 4, 107, 111, 100, 104, entity) && !lastLocation.AtEE3&& currentGoldorPhase === 2 && isInGoldor) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt EE3!`;
+    if (isPlayerInArea(0, 4, 108, 115, 98, 107, entity) && !lastLocation.AtEE3 && currentGoldorPhase === 2) {
       lastLocation.AtEE3 = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At Pre Enter 3!");
     }
-    if (isPlayerInArea(52, 56, 113, 117, 49, 53, entity) && !lastLocation.AtCore && (currentGoldorPhase === 2 || currentGoldorPhase === 3) && isInGoldor) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt Core!`;
+    if (isPlayerInArea(52, 56, 113, 117, 49, 53, entity) && !lastLocation.AtCore && (currentGoldorPhase === 2 || currentGoldorPhase === 3)) {
       lastLocation.AtCore = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At Core!");
     }
-    if (isPlayerInArea(41, 68, 110, 150, 59, 117, entity) && !lastLocation.InCore && currentGoldorPhase === 4 && isInGoldor) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eIn Core!`;
-      lastLocation.InCore = true;
-      showAlert(message);
+    if (isPlayerInArea(41, 68, 110, 150, 59, 117, entity) && !lastLocation.InGoldorTunnel && currentGoldorPhase === 4) {
+      lastLocation.InGoldorTunnel = true;
+      showAlert(playerName, playerInfo, "Inside Goldor Tunnel");
     }
-    if (isPlayerInArea(47, 61, 58, 72, 69, 83, entity) && !lastLocation.AtMid && (isInNecron)) {
-      const message = `${playerInfo.color}${playerName} (${playerInfo.className[0]}) &eAt Mid!`;
+    if (isPlayerInArea(47, 61, 64, 75, 69, 83, entity) && !lastLocation.AtMid && isInBoss("Necron")) {
       lastLocation.AtMid = true;
-      showAlert(message);
+      showAlert(playerName, playerInfo, "At Mid!");
+    }
+    if (entity.getY() < 50 && entity.getY() > 4 && !lastLocation.AtP5 && isInBoss("Necron")) {
+      lastLocation.AtP5 = true;
+      showAlert(playerName, playerInfo, "At P5!");
+    }
+    if (isPlayerInArea(91, 93, 129, 133, 44, 46, entity) && !lastLocation.Ati4Entry && isInBoss("Storm") && playerInfo.className !== "Healer") {
+      lastLocation.Ati4Entry = true;
+      showAlert(playerName, playerInfo, "At i4 Entry!");
     }
   });
-}).setFps(3);
+});
+
+register("chat", (player, message) => {
+  const name = player.replace(/\[.*?\]\s*/, ""); // remove rank
+  if (name === Player.getName()) return; // skip us
+  let msg = message.toLocaleLowerCase();
+
+  const playerInfo = getClassOf(name);
+
+  if (isValidP2(playerInfo) && validP2Msg.some(term => msg.includes(term))) {
+    lastLocation.AtP2 = true;
+    showAlert(name, playerInfo, "At P2!");
+  }
+  if (isValidAtSS() && validAtSSMsg.some(term => msg.includes(term))) {
+    lastLocation.AtSS = true;
+    showAlert(name, playerInfo, "At SS!");
+  }
+  if (isValidEE2() && validEE2Msg.some(term => msg.includes(term))) {
+    lastLocation.AtEE2 = true;
+    showAlert(name, playerInfo, "At Pre Enter 2!");
+  }
+  if (isValidEE3() && validEE3Msg.some(term => msg.includes(term))) {
+    lastLocation.AtEE3 = true;
+    showAlert(name, playerInfo, "At Pre Enter 3!");
+  }
+  if (isValidAtCore() && validAtCoreMsg.some(term => msg.includes(term))) {
+    lastLocation.AtCore = true;
+    showAlert(name, playerInfo, "At Core!");
+  }
+  if (isValidInGoldorTunnel() && validInGoldorTunnelMsg.some(term => msg.includes(term))) {
+    lastLocation.InGoldorTunnel = true;
+    showAlert(name, playerInfo, "Inside Goldor Tunnel!");
+  }
+  if (isValidAtMid() && validAtMidMsg.some(term => msg.includes(term))) {
+    lastLocation.AtMid = true;
+    showAlert(name, playerInfo, "At Mid!");
+  }
+  if (isValidAti4Entry(playerInfo) && validAti4EntryMsg.some(term => msg.includes(term))) {
+    lastLocation.Ati4Entry = true;
+    showAlert(name, playerInfo, "At i4 Entry!");
+  }
+  if (isValidAtP5() && validAtP5Msg.some(term => msg.includes(term))) {
+    lastLocation.AtP5 = true;
+    showAlert(name, playerInfo, "At P5!");
+  }
+}).setCriteria("Party > ${player}: ${message}");
 
 register("command", () => config.openGUI())
   .setName("PositionalTitles")
